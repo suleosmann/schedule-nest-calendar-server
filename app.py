dee1592ae377bc9ea84b11958199b7f722aceca4
MURIMI-ANTONY <amurimi101@gmail.com>
2024-02-20 07:05:57 +0300

    setting up flask and jwt environment

--- app.py
+++ app.py
@@ -3,3 +3,8 @@ from flask_restful import Api, Resource, reqparse
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
 from models import User
 import bcrypt
+
+app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'dingiswayo_calendar_schedular'  
+api = Api(app)
+jwt = JWTManager(app)
