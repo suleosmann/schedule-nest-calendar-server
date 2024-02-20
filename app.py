e0348e3df4218126ab7e1b7c16ad3f54a2ad2fca
MURIMI-ANTONY <amurimi101@gmail.com>
2024-02-20 07:09:43 +0300

    created Login endpoint

--- app.py
+++ app.py
@@ -8,3 +8,22 @@ app = Flask(__name__)
 app.config['JWT_SECRET_KEY'] = 'dingiswayo_calendar_schedular'  
 api = Api(app)
 jwt = JWTManager(app)
+
+
+parser = reqparse.RequestParser()
+parser.add_argument('email', type=str, required=True, help='Email is required')
+parser.add_argument('password', type=str, required=True, help='Password is required')
+
+class Login(Resource):
+    def post(self):
+        data = parser.parse_args()
+        email = data['email']
+        password = data['password']
+
+        user = User.query.filter_by(email=email).first()
+
+        if not user or not bcrypt.checkpw(password.encode('utf-8'), user.password):
+            return make_response(jsonify({'message': 'Invalid email or password'}), 401)
+
+        access_token = create_access_token(identity=email)
+        return make_response(jsonify({'access_token': access_token}), 200)
