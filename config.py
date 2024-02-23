5c47209cd9fddaacb891131a8aea7ce7e20d2007
Suleiman <suleosman73@gmail.com>
2024-02-23 09:38:58 +0300

    Fixed wrong merge

--- config.py
+++
@@ -1,4 +0,0 @@
-# config.py
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
--- run.py
+++
@@ -1,8 +0,0 @@
-# run.py
-
-from app import create_app
-
-app = create_app(port=5555, debug=True)
-
-if __name__ == '__main__':
-    app.run()
